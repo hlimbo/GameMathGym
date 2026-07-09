e70fc765c400a049639a37d412d17fdff9fd5d17
Harvey Limbo <limboh27@gmail.com>
2026-07-08 22:19:13 -0700

    convert main.cpp to use sdl app callback functions for web emscripten support | setup emscripten for cmake/vcpkg configuration specific build type

--- README.md
+++ README.md
@@ -19,24 +19,51 @@
 
 ```json
 {
-  "version": 2,
+  "version": 3,
   "configurePresets": [
     {
-      "name": "default",
-      "inherits": "vcpkg",
+      "name": "desktop-local",
+      "inherits": "desktop",
       "environment": {
-        "VCPKG_ROOT": "<path to vcpkg>"
+        "VCPKG_ROOT": "<absolute-path-to-vcpkg-folder>"
+      }
+    },
+    {
+      "name": "web-local",
+      "inherits": "web",
+      "environment": {
+        "VCPKG_ROOT": "<absolute-path-to-vcpkg-folder>",
+        "EMSDK": "<absolute-path-to-emsdk-folder>",
+        "Path": "<absolute-path-to-folder-containing-embedded-emsdk-python-executable>;$penv{Path}"
       }
     }
   ]
 }
 ```
 
+**NOTE:** For the `Path` environment variable, this will be the absolute path to the folder containing the embedded emsdk python executable. For example, the absolute path could be `C:\\Cpp\\emsdk\\python\\3.13.3_64bit` on Windows machines. This fixes an error where the embedded emsdk python version cannot be found where cmake stops the configuration step prematurely.
+
+
 How to build project using cmake?
 ```powershell
-# this builds the artifacts in the build folder and --clean-first command deletes previously built object files, libraries, and executables before starting a new build
-cmake --build build --clean-first
+# Configures using desktop preset
+cmake --preset desktop
+
+# Builds using desktop preset
+cmake --build --preset desktop-build --clean-first
+
+# Configures using web preset
+cmake --preset web-local
+
+# Builds using web preset
+cmake --build --preset web-build --clean-first
 ```
+
+What does the --build command do in cmake?
+* builds the artifacts in the build folder. Building involves running a c++ compiler to compile the code, link library dependencies, include `.dll` files required as dependencies, and ensure header files are included in order for a successful compilation. `--clean-first` command deletes previously built object files, libraries, and executables before starting a new build.
+
+How to clear build folder for fresh install?
+* delete the `build` folder from your machine by opening windows explorer and moving the `build` folder to the recycling bin.
 
 Powershell commands usage example for a hello world app
 ```bash
@@ -55,4 +82,13 @@ cmake --build build
 
 # run the app
 .\build\HelloWorld.exe
+```
+
+How to run the emscripten build on a web browser?
+* After you finish running the configuration and build steps in cmake for the web build, do the following steps:
+```powershell
+# You should have GameMathGym.html, GameMathGym.js, and GameMathGym.wasm compiled successfully by emscripten in the folder you are going to cd into
+cd build/web
+# start the python server -- access project http://localhost:8000/GameMathGym.html
+python -m http.server 8000
 ```
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <imgui_impl_sdl3.h>
 #include <imgui_impl_opengl3.h>
 
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <SDL3/SDL_opengles2.h>
@@ -9,17 +11,29 @@
 #include <SDL3/SDL_opengl.h>
 #endif
 
-
 #include <iostream>
 
 const char* WINDOW_NAME = "Game Math Gym";
+const int WIDTH = 1280;
+const int HEIGHT = 800;
 
-int main(int argc, char* argv[]) {
-  std::cout << "starting up SDL3 game window!" << std::endl;
+// state
+SDL_GLContext glContext;
+SDL_Window* win = nullptr;
+// UI State
+ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-  if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
-    std::cout << "Failed to initialize SDL VIDEO and gamepad with error: " << SDL_GetError() << std::endl;
-    return -1;
+// helper function from https://github.com/Ravbug/sdl3-sample/blob/main/src/main.cpp
+SDL_AppResult SDL_Fail() {
+  SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
+  return SDL_APP_FAILURE;
+}
+
+// https://wiki.libsdl.org/SDL3/README-main-functions
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
+    return SDL_Fail();
   }
 
   // Select GL version
@@ -48,16 +62,16 @@ int main(int argc, char* argv[]) {
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
   #endif
-  
+
   // Create window with graphics context
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
   float mainScale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
   SDL_WindowFlags windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY; 
-  SDL_Window* win = SDL_CreateWindow(WINDOW_NAME, (int)(1280 * mainScale), (int)(800 * mainScale), windowFlags);
-  
-  SDL_GLContext glContext = SDL_GL_CreateContext(win);
+  win = SDL_CreateWindow(WINDOW_NAME, (int)(WIDTH * mainScale), (int)(HEIGHT * mainScale), windowFlags);
+
+  glContext = SDL_GL_CreateContext(win);
 
   SDL_GL_MakeCurrent(win, glContext);
   SDL_GL_SetSwapInterval(1); // enable vsync
@@ -75,45 +89,56 @@ int main(int argc, char* argv[]) {
   ImGui_ImplSDL3_InitForOpenGL(win, glContext);
   ImGui_ImplOpenGL3_Init();
 
-  // UI State
-  ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  std::cout << "starting up SDL3 game window!" << std::endl;
 
-  SDL_Event e;
-  bool quit = false;
-  while (!quit) {
+  return SDL_APP_CONTINUE;
+}
 
-    while (SDL_PollEvent(&e)) {
-      // forward events to backend
-      ImGui_ImplSDL3_ProcessEvent(&e);
-      if (e.type == SDL_EVENT_QUIT) {
-        quit = true;
-      }
-    }
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
+  // forward input events into imgui
+  ImGui_ImplSDL3_ProcessEvent(event);
 
-    // Start Dear ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL3_NewFrame();
-    ImGui::NewFrame();
-    // shows what's possible with IMGUI
-    ImGui::ShowDemoWindow();
-
-    // Rendering
-    ImGui::Render();
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-    glClearColor(clearColor.x * clearColor.w, clearColor.y * clearColor.w, clearColor.z * clearColor.w, clearColor.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    SDL_GL_SwapWindow(win);
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS;
   }
 
+  return SDL_APP_CONTINUE;
+}
 
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  // Start Dear ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+  // shows what's possible with IMGUI
+  ImGui::ShowDemoWindow();
+
+  // Rendering
+  ImGui::Render();
+  ImGuiIO& io = ImGui::GetIO();
+  glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+  glClearColor(clearColor.x * clearColor.w, clearColor.y * clearColor.w, clearColor.z * clearColor.w, clearColor.w);
+  glClear(GL_COLOR_BUFFER_BIT);
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  SDL_GL_SwapWindow(win);
+
+  return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplSDL3_Shutdown();
   ImGui::DestroyContext();
 
-  SDL_GL_DestroyContext(glContext);
-  SDL_DestroyWindow(win);
+  if (glContext) {
+    SDL_GL_DestroyContext(glContext);
+  }
+
+  if (win) {
+    SDL_DestroyWindow(win);
+  }
+
   SDL_Quit();
 
-  return 0;
+  std::cout << "shutting down SDL3 game app" << std::endl;
 }
